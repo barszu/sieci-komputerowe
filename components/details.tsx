f615b3fd94498974a4bd767df271796903b73630
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2024-11-03 16:36:52 +0100

    added lab5

--- components/details.tsx
+++ components/details.tsx
@@ -1,12 +1,23 @@
-export function Details ( {children}) {
+export function Details({
+  children,
+  summary = "Kliknij, aby zobaczyć więcej",
+  ml = "1.5em",
+}) {
   return (
-<details>
-<summary><i>Odpowiedź - kliknij</i></summary>
-{ children }
-</details>
-
-
+    <details
+      style={{
+        border: "1px solid #403938",
+        borderRadius: "4px",
+        padding: "0.5em",
+        margin: `0.1em {ml}`,
+      }}
+    >
+      <summary>
+        <i>{summary}</i>
+      </summary>
+      {children}
+    </details>
   );
-};
+}
 
 export default Details;
